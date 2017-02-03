151da33c84110d7ab7d62237dbc42c6db3a21f2d
Creghton Kinney <cdkrobotics@gmail.com>
2017-02-03 16:52:52 -0500

    Added some adjustment values

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     print(file)
     testing = Speaker(file)
-    testing.adjustAmbient(10,1500)
+    testing.adjustAmbient(10,200)
     #print("Sphinx: "+testing.listenToAudioFile_Sphinx())
     #print("\nGoogle: "+testing.listenToAudioFile_Google())
     print("\n")
