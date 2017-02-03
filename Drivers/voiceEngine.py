7d6bc61370130a0a23f4e0770a1045ea1f95abfd
Creghton Kinney <cdkrobotics@gmail.com>
2017-02-03 17:00:16 -0500

    Added some adjustment values

--- Drivers/voiceEngine.py
+++ Drivers/voiceEngine.py
@@ -68,7 +68,7 @@ class Speaker(object):
             while(start <= stop):
                 self.adjustAmbient(1,50)
                 with speech_recognition.AudioFile(self.audioFile) as source:
-                    blank = self.recognizer.record(source,1,start)
+
                     audio = self.recognizer.record(source,10,start)
 
                 try:
