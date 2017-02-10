8ea843c526db68a3d2c0d36fae3afee057ddf834
Creghton Kinney <cdkrobotics@gmail.com>
2017-02-10 16:36:13 -0500

    added files, working on assistant

--- Drivers/voiceEngine.py
+++ Drivers/voiceEngine.py
@@ -4,7 +4,9 @@ class voiceEngine(object):
     def __init__(self):
         # talking engine
         self.engine = pyttsx.init('espeak')
-        self.engine.setProperty('rate', 100)
+        self.engine.setProperty('rate', 125)
+        self.engine.setProperty('age', 21)
+
 
     def speak(self, phrase):
         self.engine.say(phrase)
